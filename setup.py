9e909a7e065842c37ae755ed5ecefbdb6660b2aa
yuki <mathyassu@gmail.com>
2014-10-30 18:08:20 +0900

    dump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-__version__ = '0.3.0'
+__version__ = '0.5.0'
 classifiers = [
     "Development Status :: 4 - Beta",
     "Intended Audience :: System Administrators",
